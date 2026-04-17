41d9f3cf62d27f04d5d1e4587439540aaaa9eae0
WHO IS I <naip_naip@mail.ru>
2026-04-18 00:05:43 +0300

    db+

---
+++ dump-db-202604180005.sql
@@ -0,0 +1,294 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict PQMGcOHlAfQye26hXNcZvSCFeAjb0JtVWfOcF7EEOZixSImFvP6NyLO2k1coWpD
+
+-- Dumped from database version 18.3
+-- Dumped by pg_dump version 18.3
+
+-- Started on 2026-04-18 00:05:08
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 222 (class 1259 OID 16438)
+-- Name: course; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.course (
+    id integer NOT NULL,
+    name character varying NOT NULL,
+    path character varying NOT NULL,
+    img character varying
+);
+
+
+ALTER TABLE public.course OWNER TO postgres;
+
+--
+-- TOC entry 221 (class 1259 OID 16437)
+-- Name: course_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.course ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.course_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 226 (class 1259 OID 16497)
+-- Name: test; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.test (
+    id integer NOT NULL,
+    course_id integer NOT NULL,
+    questions text NOT NULL,
+    answer character varying NOT NULL
+);
+
+
+ALTER TABLE public.test OWNER TO postgres;
+
+--
+-- TOC entry 225 (class 1259 OID 16496)
+-- Name: test_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.test ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.test_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 220 (class 1259 OID 16424)
+-- Name: user; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."user" (
+    id integer NOT NULL,
+    name character varying NOT NULL,
+    email character varying NOT NULL,
+    password character varying NOT NULL
+);
+
+
+ALTER TABLE public."user" OWNER TO postgres;
+
+--
+-- TOC entry 224 (class 1259 OID 16449)
+-- Name: user_course; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.user_course (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    course_id integer NOT NULL
+);
+
+
+ALTER TABLE public.user_course OWNER TO postgres;
+
+--
+-- TOC entry 223 (class 1259 OID 16448)
+-- Name: user_course_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.user_course ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.user_course_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 219 (class 1259 OID 16423)
+-- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public."user" ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.user_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 5034 (class 0 OID 16438)
+-- Dependencies: 222
+-- Data for Name: course; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- TOC entry 5038 (class 0 OID 16497)
+-- Dependencies: 226
+-- Data for Name: test; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- TOC entry 5032 (class 0 OID 16424)
+-- Dependencies: 220
+-- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- TOC entry 5036 (class 0 OID 16449)
+-- Dependencies: 224
+-- Data for Name: user_course; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- TOC entry 5044 (class 0 OID 0)
+-- Dependencies: 221
+-- Name: course_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.course_id_seq', 1, false);
+
+
+--
+-- TOC entry 5045 (class 0 OID 0)
+-- Dependencies: 225
+-- Name: test_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.test_id_seq', 1, false);
+
+
+--
+-- TOC entry 5046 (class 0 OID 0)
+-- Dependencies: 223
+-- Name: user_course_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.user_course_id_seq', 1, false);
+
+
+--
+-- TOC entry 5047 (class 0 OID 0)
+-- Dependencies: 219
+-- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.user_id_seq', 1, false);
+
+
+--
+-- TOC entry 4876 (class 2606 OID 16447)
+-- Name: course course_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.course
+    ADD CONSTRAINT course_pk PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4880 (class 2606 OID 16507)
+-- Name: test test_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.test
+    ADD CONSTRAINT test_pk PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4878 (class 2606 OID 16467)
+-- Name: user_course user_course_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.user_course
+    ADD CONSTRAINT user_course_pk PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4872 (class 2606 OID 16434)
+-- Name: user user_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."user"
+    ADD CONSTRAINT user_pk PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4874 (class 2606 OID 16436)
+-- Name: user user_unique; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."user"
+    ADD CONSTRAINT user_unique UNIQUE (email);
+
+
+--
+-- TOC entry 4883 (class 2606 OID 16508)
+-- Name: test course_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.test
+    ADD CONSTRAINT course_id FOREIGN KEY (course_id) REFERENCES public.course(id) ON DELETE CASCADE;
+
+
+--
+-- TOC entry 4881 (class 2606 OID 16491)
+-- Name: user_course course_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.user_course
+    ADD CONSTRAINT course_id FOREIGN KEY (course_id) REFERENCES public.course(id) ON DELETE CASCADE;
+
+
+--
+-- TOC entry 4882 (class 2606 OID 16481)
+-- Name: user_course user_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.user_course
+    ADD CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES public."user"(id) ON DELETE CASCADE;
+
+
+-- Completed on 2026-04-18 00:05:08
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict PQMGcOHlAfQye26hXNcZvSCFeAjb0JtVWfOcF7EEOZixSImFvP6NyLO2k1coWpD
+
